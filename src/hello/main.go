d20695c1d156ddc63928db7420ff303b89cf86d8
warun <warun@ubinix.com>
2016-07-26 06:07:05 +0700

    try to add web-app with github.com/ant0ine/go-json-rest

--- src/hello/main.go
+++ src/hello/main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"echo"
 	"fmt"
+	"github.com/ant0ine/go-json-rest/rest"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(echo.Echo("hello world"))
+	fmt.Println(":8080 'hello world'")
+
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
+		w.WriteJson(map[string]string{"Body": echo.Echo("hello world")})
+	}))
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
