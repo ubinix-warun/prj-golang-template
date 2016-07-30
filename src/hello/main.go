1b4305146f328b29cd6d1e4a83d35fd0c9a93ea1
warun <warun@ubinix.com>
2016-07-30 12:13:21 +0700

    read json config from var-memory,clean api and handle

--- src/hello/main.go
+++ src/hello/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
 func echoHandler() http.HandlerFunc {
@@ -13,8 +14,51 @@ func echoHandler() http.HandlerFunc {
 		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	}
 }
+
+type Link struct {
+	Url string
+}
+
+type Config struct {
+	Links []Link
+}
+
+var in = `{
+    "links": [
+        {
+            "Url": "127.0.0.1:17001"
+        },
+        {
+            "Url": "127.0.0.1:17002"
+        }
+    ]
+}`
+
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("view")
+	var conf Config
+	err := json.Unmarshal([]byte(in), &conf)
+
+	if err != nil {
+		fmt.Print("Error:", err)
+	}
+
+	fmt.Printf("%#v\n", conf)
+	fmt.Printf("%s\n",conf.Links[0].Url)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r)
+	}
+}
+
 func main() {
-	fmt.Println(":8080/hello 'hello world'")
+	fmt.Println(":8080/api/.status")
+	fmt.Println(":8080/api/hello 'hello world'")
+	fmt.Println(":8080/echo/hello/john ...")
+	fmt.Println(":8080/ file")
 
 	api := rest.NewApi()
 	statusMw := &rest.StatusMiddleware{}
@@ -35,7 +79,8 @@ func main() {
 
 	api.SetApp(router)
 	http.Handle("/api/", http.StripPrefix("/api", api.MakeHandler()))
-	http.Handle("/echo", http.StripPrefix("/", echoHandler()))
+	http.Handle("/echo/", http.StripPrefix("/echo", echoHandler()))
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	http.HandleFunc("/view", makeHandler(viewHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
